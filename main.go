fee46b19430ee7d6ed71cc87a57fb497e54a3e80
hailaz <739476267@qq.com>
2020-01-17 11:30:23 +0800

    优化配置读取判断

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/os/gcmd"
+	"github.com/gogf/gf/os/gfile"
+	"github.com/gogf/gf/os/glog"
 	"github.com/hailaz/gadmin/app/model"
 	"github.com/hailaz/gadmin/library/logger"
 	"github.com/hailaz/gadmin/library/timer"
@@ -9,12 +12,26 @@ import (
 )
 
 func init() {
+	cfg := gcmd.GetOpt("c", "config.default.toml")
+
+	if !gfile.Exists(cfg) && !gfile.Exists("config/"+cfg) {
+		glog.Fatalf("config file(%s) no exist", cfg)
+	}
+
 	// 设置默认配置文件，默认的 config.toml 将会被覆盖
-	g.Config().SetFileName("config.toml")
-	// 初始化数据库
-	model.InitModel()
+	g.Config().SetFileName(cfg)
+
 	// 初始化日志
 	logger.InitLogger()
+
+	// 测试数据库
+	err := g.DB().PingMaster()
+	if err != nil {
+		glog.Fatalf("DB err:%v", err)
+	}
+
+	// 初始化数据库
+	model.InitModel()
 
 	timer.InitTimer()
 
